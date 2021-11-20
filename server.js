562bb242d30faafae92123eb0a07c22c9e8fa55d
Suryaobulareddy <33251273+Suryaobulareddy@users.noreply.github.com>
2021-11-20 19:14:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ var macs = ()=>{
 
 console.log( macs() );//create a server object:
 http.createServer(function (req, res) {
-  res.write('Welcome to Surya {'+macs().toString()+' }'); //write a response to the client
+  res.write('Welcome to IOTronics, Desined by Surya ::-) {'+macs().toString()+' }'); //write a response to the client
   res.end(); //end the response
 }).listen(8100); //the server object listens on port 8080
